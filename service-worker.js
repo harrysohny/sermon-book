a43082256dfa3fe7bdf0c55bd828a614913527d9
Harry Sohn <shlark@gmail.com>
2026-09-01 11:11:45 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const C='sermons-v2';const A=['./','index.html','style.css','app.js','manifest.json','data/sermons.json','images/sermon100.jpg','images/sermon101.jpg','images/sermon102.jpg','icons/icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
